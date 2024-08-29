b86938bcc48cd99ef2cd580932fd1f14a2c13d5b
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-29 10:41:05 +0700

    05 vardump bertipe objeck

---
+++ 05 vardump bertipe objeck.php
@@ -0,0 +1,14 @@
+<?php
+class Mobil {
+    public $merk;
+    public $model;
+}
+
+$mobil_baru = new Mobil();
+$mobil_baru->merk = "MItsubishi";
+$mobil_baru->model = "Panther";
+
+var_dump($mobil_baru);
+
+print_r("<i><p><strong>By  : Ahmad arjun trisula");
+?>
