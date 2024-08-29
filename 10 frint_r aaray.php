0f24dbadd0b16cef41aa02ad46ab2a2113914df8
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-29 11:32:52 +0700

    10 printf dan vardump dari literatur luar

---
+++ 10 frint_r aaray.php
@@ -0,0 +1,20 @@
+<?php
+$array = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
+
+$array = array(
+    'name' => 'Michael jackson',
+    'age' => 20,
+    'skills' => array('PHP', 'JavaScript', 'CSS')
+);
+
+var_dump($array);
+
+print "<p>";
