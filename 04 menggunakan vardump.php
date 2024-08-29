00a038ad218d47644e329c0e4866e66a84f2891c
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-29 10:34:30 +0700

    04 menggunakan vardump

---
+++ 04 menggunakan vardump.php
@@ -0,0 +1,13 @@
+<?php
+$variabel_string = "Halo Nusantara!";
+$variabel_integer = 123;
+$variabel_array = array(1, 2, "tiga", 4.5);
+
+var_dump($variabel_string);
+var_dump($variabel_integer);
+var_dump($variabel_array);
+
+
+
+print_r("<i><p><strong>By  : Ahmad arjun trisula");
+?>
