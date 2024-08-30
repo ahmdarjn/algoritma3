3f391e41060ef2a4bfc3c367ef3b0ad3ddfaec47
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 10:09:34 +0700

    51 pernyataan switch

---
+++ 51 Pernyataan Switch.php
@@ -0,0 +1,15 @@
+<?php
+$grade = 'B';
+switch ($grade) {
+  case 'A':
+    echo "Luar biasa!";
+    break;
+  case 'B':
+  case 'C':
+    echo "Bagus!";
+    break;
+  default:
+    echo "Tetap semangat!";
+}
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
