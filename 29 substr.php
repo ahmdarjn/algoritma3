ccde439c838ed9f095c2b1617b66765b08d25587
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 09:02:55 +0700

    29 substr

---
+++ 29 substr.php
@@ -0,0 +1,6 @@
+<?php
+$text = "Hello World!";
+echo substr($text, 0, 5); // Outputs: "Hello"
+echo substr($text, 6); // Outputs: "World!"
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
