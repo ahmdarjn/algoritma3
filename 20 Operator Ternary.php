f6c78c1f1e503328db3980e11a259ea506ab8663
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 08:42:52 +0700

    20 operator ternary

---
+++ 20 Operator Ternary.php
@@ -0,0 +1,11 @@
+<?php
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak";
+
+// menampilkan jawaban
+echo $jawab;
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
