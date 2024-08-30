a1ad455cdae7ba6e769650902323e2fe45e4252d
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 09:23:40 +0700

    41 percabangan if

---
+++ 41 percabangan if.php
@@ -0,0 +1,15 @@
+<?php
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+    echo "Anda dapat hadiah!";
+}
+
+<?php
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+    echo "Anda dapat hadiah!";
+}
+
+print_r("<p><strong><i>By Meylisa eka putry</strong>");
