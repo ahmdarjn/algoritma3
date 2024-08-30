0c0ca1cc1cc2250d2070bc3001719b18cccebef2
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 10:19:26 +0700

    contoh penggunan if dan else

---
+++ 56 Contoh Penggunaan If dan Else.php
@@ -0,0 +1,11 @@
+<?php
+$umur = 16;
+
+if ($umur >= 17) {
+    echo "Kamu sudah cukup umur.";
+} else {
+    echo "Kamu belum cukup umur.";
+}
+
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
