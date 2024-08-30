f2f3f8d3cf4b6f882185f1afe32c9cf53d57cdef
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 08:52:11 +0700

    24 strtoupper dan strtolower

---
+++ 24 strtoupper dan strtolower.php
@@ -0,0 +1,5 @@
+<?php
+echo strtoupper("Hello World!"); // Outputs: "HELLO WORLD!"
+echo strtolower("Hello World!"); // Outputs: "hello world!"
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
