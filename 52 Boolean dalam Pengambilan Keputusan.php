b2b680727ddca2f84ff5716af6650b102b539618
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 10:11:55 +0700

    52 Boolean dalam Pengambilan Keputusan

---
+++ 52 Boolean dalam Pengambilan Keputusan.php
@@ -0,0 +1,10 @@
+<?php
+$login_sukses = true;
+
+if ($login_sukses) {
+    echo "Selamat datang pengguna!";
+} else {
+    echo "Login gagal, silakan coba lagi.";
+}
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
