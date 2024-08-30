3e70bf587ca17c1a69440e239908cafb02207806
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 08:31:20 +0700

    15 operator penugasan

--- 14 Parentheses (tanda kurung).php
+++ 14 Parentheses (tanda kurung).php
@@ -1,5 +1,5 @@
 <?php
-$hasil = (7 + 7) * 3; // Hasilnya adalah 20, bukan 15
+$hasil = (7 + 7) * 3; // Hasilnya adalah 42, bukan 27
 echo "hasil = $hasil";
 
 print_r("<i><p><strong><i>By :Ahmad arjun trisula");
---
+++ 15 Operator Penugasan (Assignment).php
@@ -0,0 +1,16 @@
+<?php
+$speed = 83;
+
+// ini opertor aritmatika
+$speed = $speed + 10;
+
+// maka nilai speed akan samadengan 83 + 10 = 93
+
+// ini operator penugasan
+$speed += 10;
+
+// sekarang nilai speed akan menjadi 93 + 10 = 103
+
+echo "hasil speed = $speed";
+
+print_r("<i><p><strong><i>By : Ahmad arjun trisula</strong>");
