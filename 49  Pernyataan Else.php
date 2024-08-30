b4092e4dd639f866eeb60d8de27a639282f6881d
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 10:04:07 +0700

    49 pernyataan else

---
+++ 49  Pernyataan Else.php
@@ -0,0 +1,9 @@
+<?php
+$nilai = 60;
+if ($nilai > 70) {
+  echo "Selamat, kamu lulus!";
+} else {
+  echo "Maaf, kamu harus mengulang.";
+}
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
