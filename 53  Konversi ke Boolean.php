4f18a8274a60080183efde8981e8ddbd7fd5d5af
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 10:13:52 +0700

    53 konversi ke bolean

---
+++ 53  Konversi ke Boolean.php
@@ -0,0 +1,11 @@
+<?php
+$nilai = 0;
+
+if ($nilai) {
+    echo "Variabel ini bernilai true";
+} else {
+    echo "Variabel ini bernilai false";
+}
+// Keluaran akan "Variabel ini bernilai false" karena nilai 0 dianggap false.
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
