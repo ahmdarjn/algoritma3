3b3dc2e9f024407d1bdf9191d4b22d461f29083f
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 09:46:25 +0700

    43 mengubah nilai umur

---
+++ 43 mengubah nilai umur.php
@@ -0,0 +1,14 @@
+<?php
+
+$umur = 19;
+
+if ($umur < 18 ){
+    echo "Kamu tidak boleh membuka situs ini!";
+} else {
+    echo "Selamat datang di website kami!";
+}
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
+
+
+?>
