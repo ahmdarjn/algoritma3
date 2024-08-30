a3152232aa5a5ffd54383e0b0f92e08aeb68f092
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 10:00:08 +0700

    47 percabangan bersarang

---
+++ 47 Percabangan Bersarang.php
@@ -0,0 +1,16 @@
+<?php
+$umur = 17;
+$menikah = false;
+
+if($umur > 18){
+    if($menikah){
+        echo "<br>Selamat datang pak!";
+    } else {
+        echo "<br>Selamat datang wahai pemuda!";
+    }
+} else {
+    echo "Maaf website ini hanya untuk yang sudah berumur 18+";
+}
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
+
+?>
