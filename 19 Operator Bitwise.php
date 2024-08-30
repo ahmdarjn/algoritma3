a7a2ed2956245a104f47bbd49d2eff28900e0604
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 08:40:58 +0700

    19 operator bitwise

---
+++ 19 Operator Bitwise.php
@@ -0,0 +1,31 @@
+<?php
+
+$a = 30;
+$b = 13;
+
+// bitwise AND
+$c = $a & $b;
+echo "$a & $b = $c";
+echo "<br>";
+
+// bitwise OR
+$c = $a | $b;
+echo "$a | $b = $c";
+echo "<br>";
+
+// bitwise XOR
+$c = $a ^ $b;
+echo "$a ^ $b = $c";
+echo "<br>";
+
+// Shift Left
+$c = $a << $b;
+echo "$a << $b = $c";
+echo "<br>";
+
+// Shift Right
+$c = $a >> $b;
+echo "$a >> $b = $c";
+echo "<br>";
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
