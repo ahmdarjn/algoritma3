a22629d95474b23596e6270a209aaed9042b1267
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 09:04:56 +0700

    30 strcmp dan strcasecmp

---
+++ 30 strcmp dan strcasecmp.php
@@ -0,0 +1,5 @@
+<?php
+echo strcmp("Hello World!", "Hello World!"); // Outputs: 0
+echo strcasecmp("hello world!", "HELLO WORLD!"); // Outputs: 0
+
+print_r("<i><p><strong><i>By  :Ahmad arjun trisula</strong>");
