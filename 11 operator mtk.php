c2ee2b8c1c11883bcf956474e1e63ac33f5854d6
ahmdarjn <ahmadarjun347@gmail.com>
2024-08-30 08:20:27 +0700

    11 operator matematika

---
+++ 11 operator mtk.php
@@ -0,0 +1,38 @@
+<?php
+
+$a = 5;
+$b = 1;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+// Perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+// Pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+// modulus Sisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+// Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+
+print_r("<i><p><strong><i>By : Ahmad arjun trisula</strong>");
+
+?>
